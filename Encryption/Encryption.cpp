18d637acfe07b068162dc8fcce503cf737f381d0
Masha-bat <batyushkova03@mail.ru>
2022-12-09 15:12:31 +0300

    Добавили шаблон

--- Encryption/Encryption.cpp
+++ Encryption/Encryption.cpp
@@ -9,8 +9,21 @@
 using namespace std;
 using namespace chrono;
 
+template <typename T>
+void run(string text, T func_name, Encryption p)
+{
+
+	auto start = steady_clock::now();
+	func_name(text, p);
+	auto end = steady_clock::now();
+	auto elapsed_ms = duration_cast<microseconds>(end - start);
+	cout << elapsed_ms.count() << " microsec" << endl;
+}
+
+
 int main()
 {
+	string text = "ddbufhsvdfhvvbdgvhf";
 	Encryption p;
-	Galimov(p);
+	//run(text, Hill, p);
 }
