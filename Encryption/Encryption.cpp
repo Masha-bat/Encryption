7d8dc074b0a6997af4ea130be982fb485308e2ea
Masha-bat <batyushkova03@mail.ru>
2022-12-09 14:14:02 +0300

    Начальные данные
    Создание .cpp и .h

---
+++ Encryption/Encryption.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <chrono>
+#include "Header.h"
+
+using namespace std;
+using namespace chrono;
+
+int main()
+{
+
+}
---
+++ Encryption/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Encryption
+{
+	string keyword = "";
+	int a = 0, b = 0, c = 0, d = 0;
+};
